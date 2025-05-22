f16c7c418e969c7fcc9385f114a274015d7bc0aa
BONNV <bonnv@vega.com.vn>
2025-05-22 09:57:04 +0700

    add:api-login

--- src/main/java/com/example/demo/controllers/AuthenticationController.java
+++ src/main/java/com/example/demo/controllers/AuthenticationController.java
@@ -1,20 +1,24 @@
 package com.example.demo.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.dto.ApiResponse;
 import com.example.demo.jwt.JwtUtil;
 import com.example.demo.models.User;
-import com.example.demo.models.response.JwtResponse;
 import com.example.demo.service.MyUserDetailsService;
+
+import java.util.Collections;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -32,20 +36,57 @@ public class AuthenticationController {
     @Autowired
     private MyUserDetailsService userDetailsService;
     
-    private final Logger logger = LoggerFactory.getLogger(AuthenticationController.class);
+    private final Logger logger = LoggerFactory.getLogger(getClass());
 
     @PostMapping("/login")
-    public ResponseEntity<?> createAuthenticationToken(@RequestBody User authenticationRequest) throws Exception {
-    	logger.info("Đang chạy 1");
-        Authentication authentication = authenticationManager.authenticate(
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody User authenticationRequest) {
+        try {
+            logger.info("LoggerService will run in 3s");
+            authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword())
-        );
-    	logger.info("Đang chạy 2");
+            );
 
-        final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
-    	logger.info("Đang chạy 3");
-        final String jwt = jwtUtil.generateToken(userDetails);
-    	logger.info("Đang chạy 4");
-        return ResponseEntity.ok(new JwtResponse(jwt));
+            final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+
+            ApiResponse<Object> response = new ApiResponse<>(
+                false,
+                "User không tồn tại",
+                "404",
+                null
+            );
+
+            if (userDetails != null) {
+                final String jwt = jwtUtil.generateToken(userDetails);
+
+                response = new ApiResponse<>(
+                    true,
+                    "Đăng nhập thành công",
+                    "200",
+                    Collections.singletonMap("token", jwt)
+                );
+            } 
+
+            return ResponseEntity.status(HttpStatus.OK).body(response);
+        } catch (AuthenticationException e) {
+            logger.error("====== AuthenticationException ERROR ======");
+            logger.error("Chi tiết lỗi: ", e);
+            ApiResponse<Object> response = new ApiResponse<>(
+                false,
+                "Sai thông tin đăng nhập",
+                "404",
+                null
+            );
+            return ResponseEntity.status(HttpStatus.OK).body(response);
+        } catch (Exception e) {
+            logger.error("====== UNEXPECTED ERROR ======");
+            logger.error("Chi tiết lỗi: ", e);
+            ApiResponse<Object> response = new ApiResponse<>(
+                false,
+                "Có lỗi xảy ra, vui lòng thử lại",
+                "500",
+                null
+            );
+            return ResponseEntity.status(HttpStatus.OK).body(response);
+        }
     }
 }
---
+++ src/main/java/com/example/demo/dto/ApiResponse.java
@@ -0,0 +1,49 @@
+package com.example.demo.dto;
+
+public class ApiResponse<T> {
+    private boolean success;
+    private String message;
+    private String code;
+    private T data;
+
+    public ApiResponse() {}
+
+    public ApiResponse(boolean success, String message, String code, T data) {
+        this.success = success;
+        this.message = message;
+        this.code = code;
+        this.data = data;
+    }
+
+	public boolean isSuccess() {
+		return success;
+	}
+
+	public void setSuccess(boolean success) {
+		this.success = success;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public T getData() {
+		return data;
+	}
+
+	public void setData(T data) {
+		this.data = data;
+	}
+}
